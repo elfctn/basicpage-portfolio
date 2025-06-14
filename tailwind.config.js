4b745ad7135153000aaf1b48a0a90b602df2799c
Elif Cetin <elifcetin.dev@gmail.com>
2025-06-14 23:23:18 +0300

    refactor(css): Clean up and optimize global CSS in index.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{html,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      // Font ailelerini buraya ekliyoruz
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-  darkMode: 'class',
-  
-}
+  darkMode: "class",
+};
